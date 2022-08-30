10012746d17b55c3415d103d329c0c60f285a1c4
Xcube_bhanumandharam-at-690341952381 <bhanu.mandharam@xcubelabs.com>
2022-08-30 13:11:08 +0530

    added form validation messages in Suignup

--- src/app/views/Auth/sign-up/sign-up.component.ts
+++ src/app/views/Auth/sign-up/sign-up.component.ts
@@ -12,78 +12,97 @@ import { AppTokens, CommonMessages } from 'src/app/shared/constants/constants/co
 @Component({
   selector: 'app-signup',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignupComponent extends BaseClass implements OnInit {
   public signUpForm: FormGroup | any;
   localStorageService: any;
 
   signupValidationMessages = {
-    firstName: [
+    first_name: [
       {
-        type: 'required', message: 'First Name is Required'
-      }
+        type: 'required',
+        message: 'First Name is Required',
+      },
     ],
-    phoneNumber : [
+    last_name: [
       {
-        type : 'required', message: 'Phone Number is Required'
+        type: 'required',
+        message: 'Last Name is Required',
+      },
+    ],
+    email: [
+      {
+        type: 'required',
+        message: 'Email is Required',
+      },
+    ],
+    phone_number: [
+      {
+        type: 'required',
+        message: 'Phone Number is Required',
       },
       {
-        type : 'pattern' , message: 'Please Enter a Valid Mobile Number'
+        type: 'pattern',
+        message: 'Please Enter a Valid Mobile Number',
+      },
+    ],
+    password: [
+      {
+        type: 'required',
+        message: 'Password is Required',
       }
-    ]
-  }
+    ],
+  };
 
   constructor(
     private loginService: LoginService,
     private _fb: FormBuilder,
     private commonRequestService: CommonRequestService,
-    private toastr:ToastrService,
-    private router:Router
+    private toastr: ToastrService,
+    private router: Router
   ) {
     super();
     this.initializeForm();
   }
 
-
-
   signup(value: any) {
-
     const requestPayload = { ...value };
     this.commonRequestService
       .request(RequestEnums.SIGNUP, requestPayload)
-      .subscribe((response) =>{
+      .subscribe((response) => {
         this.toastr.success(CommonMessages.SIGNUP_SUCESS);
         this.router.navigate(['/auth/login']);
-      })
+      });
   }
 
-
   /**
-     * Function to initialize the form
-     */
+   * Function to initialize the form
+   */
   initializeForm() {
     this.signUpForm = new FormGroup({
-      first_name: new FormControl('', [
-        Validators.required]),
-      last_name: new FormControl('', [
-        Validators.required]),
+      first_name: new FormControl('', [Validators.required]),
+      last_name: new FormControl('', [Validators.required]),
       email: new FormControl('', [
         Validators.required,
         Validators.email,
         CustomValidators.noWhitespaceValidator,
       ]),
       password: new FormControl('', [
-        Validators.required, Validators.maxLength(100)]),
+        Validators.required,
+        Validators.maxLength(100),
+      ]),
       phone_number: new FormControl('', [
-        Validators.required, Validators.maxLength(13)]),
+        Validators.required,
+        Validators.maxLength(13),
+      ]),
       user_type: new FormControl('3'),
       google_login_id: new FormControl(''),
       google_login_status: new FormControl('0'),
       facebook_login_id: new FormControl(''),
       facebook_login_status: new FormControl('0'),
       remember_me: new FormControl('0'),
-      status: new FormControl('0')
+      status: new FormControl('0'),
     });
   }
 }
