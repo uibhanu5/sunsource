53a6a525d1e69278da9bba3039c6212397305407
@Venugopal <venugopal@g2techsoft.com>
2022-09-06 09:41:33 +0530

    Sidebar is done need to implement uploads

--- src/app/views/Admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/views/Admin/admin-sidebar/admin-sidebar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSidebarComponent implements OnInit {
 
+  public sidebarShow: boolean = false;
   constructor() { }
 
   ngOnInit() {
